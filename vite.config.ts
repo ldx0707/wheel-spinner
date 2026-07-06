9bc4377b642dcefa57fec7fc8ae837db088250f5
ldx <843362892@qq.com>
2026-07-06 20:59:20 +0800

    Fix base path and SW registration for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/wheel-spinner/',
   plugins: [react()],
   server: {
     host: '0.0.0.0',
